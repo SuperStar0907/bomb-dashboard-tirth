efaffd2d5324e80559a4fb3b164d903f4214d455
Aaron Shames <aaron@1coin.io>
2022-06-02 14:41:08 -0400

    farm update

--- src/bomb-finance/BombFinance.ts
+++ src/bomb-finance/BombFinance.ts
@@ -446,21 +446,21 @@ export class BombFinance {
     }
     const rewardPerSecond = await poolContract.tSharePerSecond();
     if (depositTokenName.startsWith('BOMB-BTCB')) {
-      return rewardPerSecond.mul(400).div(1000);
+      return rewardPerSecond.mul(400).div(875);
     } else if (depositTokenName.startsWith('BOMB-BSHARE')) {
       return rewardPerSecond.mul(0).div(1000);
     } else if (depositTokenName.startsWith('BOMB')) {
       return rewardPerSecond.mul(0).div(1000);
     } else if (depositTokenName.startsWith('BBOND')) {
-      return rewardPerSecond.mul(150).div(1000);
+      return rewardPerSecond.mul(150).div(875);
     } else if (depositTokenName.startsWith('BUSM-BUSD')) {
-      return rewardPerSecond.mul(50).div(1000);
+      return rewardPerSecond.mul(50).div(875);
     } else if (depositTokenName.startsWith('80BOMB')) {
-      return rewardPerSecond.mul(200).div(1000);
+      return rewardPerSecond.mul(100).div(875);
     } else if (depositTokenName.startsWith('80BSHARE')) {
-      return rewardPerSecond.mul(50).div(1000);
+      return rewardPerSecond.mul(25).div(875);
     } else {
-      return rewardPerSecond.mul(150).div(1000);
+      return rewardPerSecond.mul(150).div(875);
     }
     // if (depositTokenName.startsWith('BOMB-BTCB')) {
     //   return rewardPerSecond.mul(41650).div(10000);
